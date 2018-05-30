88295d984cd5d97f570fd833b130c930adc183ed
IN000402 <IN000402@DE227.idiadagroup.com>
2018-05-30 08:57:23 +0200

    Circle area logic

--- shape-calculator/src/main/java/com/idiada/Rectangle.java
+++ shape-calculator/src/main/java/com/idiada/Rectangle.java
@@ -6,8 +6,9 @@ public class Rectangle implements Shape {
 	Scanner sc = new Scanner(System.in);
 	
 	public void calculateArea(){
-		System.out.println("Enter side length");
+		System.out.println("Enter Breadth of Rectangle");
 		float breadth = sc.nextFloat();
+		System.out.println("Enter Length of Rectangle");
 		float length = sc.nextFloat();
 		float area =  breadth * length;
 		System.out.println("Area of Rectangle is : " +area);
--- shape-calculator/src/main/java/com/idiada/ShapeCalculator.java
+++ shape-calculator/src/main/java/com/idiada/ShapeCalculator.java
@@ -11,18 +11,32 @@ public class ShapeCalculator {
 	
 	public static void main(String[] args) {
 		Shape shape = null;
-		System.out.println("Select Shape from the below options");
-		System.out.printf("1. Square\n2. Rectangle");
-		switch (sc.nextInt()) {
+		int choice=0;
+		
+		do{ 
+			System.out.println("Select Shape from the below options");
+			System.out.printf("1. Square\n2. Rectangle\n3. Circle\n0. Exit");
+			switch (choice=sc.nextInt()) {
 		case 1:
 			shape = new Square();
+			shape.calculateArea();
 			break;
 		case 2:
 			shape = new Rectangle();
+			shape.calculateArea();
+			break;
+		case 3:
+			shape = new Circle();
+			shape.calculateArea();
+			break;
+		case 0:
+			System.out.println("Closing Calculator");
 			break;
 		default:
 			System.out.println("Enter option number from above list");
 		}
-		shape.calculateArea();
+		}
+		while(choice!=0);
+		System.exit(0);
 	}
 }
